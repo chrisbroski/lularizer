189d2f5c862f48abe1487166a445c510d86c7a09
Chris Broski <chrisbroski@gmail.com>
2016-09-13 19:58:55 -0400

    script to run main lib

---
+++ run.py
@@ -0,0 +1,20 @@
+import lularize
+
+source = "/Users/christopherbroski/Desktop/lularize/"
+watermark = "LuLaRoe Stacy Leasure-Broski"
+
+print 'Left-to-right position of the closeup (0 - 100) or hit enter for 50'
+left_pos = raw_input('> ')
+
+if left_pos == '':
+    left_pos = 50
+
+print 'Top-to-bottom position of the closeup (0 - 100) or hit enter for 50'
+top_pos = raw_input('> ')
+
+if top_pos == '':
+    top_pos = 50
+
+detail = [int(left_pos), int(top_pos)]
+
+lularize.processFolder(source, watermark, (246, 117, 153), detail, False, False)
